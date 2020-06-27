8111e78625ec37dfeb80f2868e174e4cc3c4c7a8
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-06-27 10:53:11 +0800

    [El-Corona] Display possible infected human using Closed Relation graph GUI by Joo Han @ 270620 12:00

---
+++ src/ContactTracer/ClosedTree.java
@@ -0,0 +1,35 @@
+
+package ContactTracer;
+
+import java.text.ParseException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
+import static javafx.application.Application.launch;
+
+public class ClosedTree extends Application{
+    
+    private Text data;
+    public static Tracer TRACER;
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public void start(Stage primaryStage) throws ParseException{
+        primaryStage.setWidth(700);
+        primaryStage.setHeight(500);
+        data = new Text();
+        data.setText(TRACER.ClosedTreeData);
+        TextFlow textFlow = new TextFlow();
+        textFlow.getChildren().add(data);
+        ScrollPane pane = new ScrollPane(textFlow);
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+}
