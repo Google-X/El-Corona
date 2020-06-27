45b8835d16d82501a74c1c75def5ac627fb167c6
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-06-27 10:42:06 +0800

    [El-Corona] ListNode for LinkedList @ 270620 12:00

---
+++ src/SourceDS/ListNode.java
@@ -0,0 +1,44 @@
+/*
+ * ListNode for all classes in Lab 4 questions.
+ */
+package SourceDS;
+
+public class ListNode<E> {
+    private E data;
+    private ListNode link;
+
+    public ListNode(E data, ListNode link) {
+        this.data = data;
+        this.link = link;
+    }
+    
+    public ListNode() {
+        data = null;
+        link = null;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public ListNode getLink() {
+        return link;
+    }
+
+    public void setLink(ListNode link) {
+        this.link = link;
+    }
+
+    @Override
+    public String toString() {
+        return data + " --> ";
+    }
+    
+    public String musicPlayerToString(){
+        return "Music : " + data + " --> ";
+    }
+}
